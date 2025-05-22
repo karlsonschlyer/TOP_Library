facebb494c4a12f641816ba4f58b5e193ef7a1b8
Karlson Schlyer <42825291+karlsonschlyer@users.noreply.github.com>
2025-05-21 21:11:20 -0500

    Add New Book button and implement displaying of book data

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const myLibrary = [];
 
-function Book(id, title, author, published) {
+function Book(id, title, author, read) {
     this.id = id;
     this.title = title;
     this.author = author;
-    this.published = published;
+    this.read = read;
 }
 
-function addBook(title, author, published) {
-    var newBook = new Book(crypto.randomUUID(), title, author, published);
+function addBook(title, author, read) {
+    var newBook = new Book(crypto.randomUUID(), title, author, read);
 
     myLibrary.push(newBook);
 }
 
+const bookContainer = document.querySelector(".bookContainer");
+
+
+addBook("Test Title", "Author", true);
+
+for (var book of myLibrary) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
+
+    bookCard.textContent += book.title + "\n";
+    bookCard.textContent += book.author + "\n";
+    bookCard.textContent += book.read + "\n";
+    bookCard.textContent += book.id + "\n";
+
+    bookContainer.appendChild(bookCard);
+
+}
