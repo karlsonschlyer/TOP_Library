eca46f6ba0a85a11e0efb34389395bd94f8a1f03
Karlson Schlyer <42825291+karlsonschlyer@users.noreply.github.com>
2025-05-22 17:07:39 -0500

    Implement functionality for adding new books using form dialog

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const myLibrary = [];
 const bookContainer = document.querySelector(".bookContainer");
 const bookDialog = document.querySelector("dialog");
 const newBookButton = document.querySelector("#newBook");
+const form = document.querySelector("form");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const readInput = document.querySelector("#read");
 
 function Book(id, title, author, read) {
     this.id = id;
@@ -17,22 +21,35 @@ function addBook(title, author, read) {
     myLibrary.push(newBook);
 }
 
+function displayLibrary() {
+    for (var book of myLibrary) {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("bookCard");
+
+        bookCard.textContent += "Title: " + book.title + "\n";
+        bookCard.textContent += "Author: " + book.author + "\n";
+        bookCard.textContent += "Book has been read?: " + book.read + "\n";
+        bookCard.textContent += book.id + "\n";
+
+        bookContainer.appendChild(bookCard);
+
+    }
+}
+
+form.addEventListener("submit", formSubmit);
+function formSubmit(event) {
+    event.preventDefault();
+    
+    addBook(titleInput.value, authorInput.value, readInput.checked);
+    while (bookContainer.firstChild) {
+        bookContainer.removeChild(bookContainer.firstChild);
+    }
+    bookDialog.close();
+    displayLibrary();
+}
+
 newBookButton.addEventListener("click", () => {
-    bookDialog.show();
+    bookDialog.showModal();
 });
 
-
-addBook("Test Title", "Author", true);
-
-for (var book of myLibrary) {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("bookCard");
-
-    bookCard.textContent += book.title + "\n";
-    bookCard.textContent += book.author + "\n";
-    bookCard.textContent += book.read + "\n";
-    bookCard.textContent += book.id + "\n";
-
-    bookContainer.appendChild(bookCard);
-
-}
+displayLibrary();
